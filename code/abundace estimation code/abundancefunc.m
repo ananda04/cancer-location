ad6b938f6a769ca501ab9f8765a942f8237415d8
ananda04 <46113015+ananda04@users.noreply.github.com>
2024-06-05 23:05:07 -0400

    Update abundancefunc.m

--- code/abundace estimation code/abundancefunc.m
+++ code/abundace estimation code/abundancefunc.m
@@ -1,3 +1,6 @@
+% Author:      Arnav Nanda 
+%              Duke University, Pratt School of Engineering 
+
 function [w] = abundancefunc(pixel)
     load('ReconResults_Model_3_BrainScan_GBMT_FullFanExtent_400iter_M3.mat')
 
