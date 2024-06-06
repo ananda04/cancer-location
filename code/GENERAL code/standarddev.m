04f6e21a998ad8e72212e2065a2963eb93f64df8
ananda04 <46113015+ananda04@users.noreply.github.com>
2024-06-05 22:58:05 -0400

    Update standarddev.m

--- code/GENERAL code/standarddev.m
+++ code/GENERAL code/standarddev.m
@@ -1,3 +1,6 @@
+% Author:      Arnav Nanda 
+%              Duke University, Pratt School of Engineering 
+
 A = [squeeze(reconstructedData(87,11,:)),squeeze(reconstructedData(89,15,:)),squeeze(reconstructedData(81,20,:)),squeeze(reconstructedData(85,20,:)),squeeze(reconstructedData(88,19,:)),squeeze(reconstructedData(89,14,:)),squeeze(reconstructedData(72,28,:)),squeeze(reconstructedData(84,25,:)),squeeze(reconstructedData(84,21,:)),squeeze(reconstructedData(89,16,:))]
 B = transpose(A)
 C = mean(B)
